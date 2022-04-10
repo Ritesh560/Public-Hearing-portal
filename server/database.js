e23432b4943c380ab27a4e401684e106e83827b3
Ritesh Kumar <singlaritesh007@gmail.com>
2022-04-10 20:22:25 +0530

    security

--- server/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize, Datatype } = require("sequelize")
-
-const sequelize = new Sequelize("public-hearing", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
-})
-
-module.exports = sequelize
